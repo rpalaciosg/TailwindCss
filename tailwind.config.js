35201b1150af2783c248eb1c041f43f884c4a0f2
Richard Palacios <rpalacios@mutazuay.com>
2025-02-28 16:11:25 -0500

    feat(responsive): uso de break-points en los elementos dentro del body

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ export default {
         'azul-claro':'#243cff',
         'azul-oscuro': '#0d1b3e',
       },
-      width:{
-        '42':'170px',
+      screens:{
+        'tablet': '900px',
       },
     },
   },
